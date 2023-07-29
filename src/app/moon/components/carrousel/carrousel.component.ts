828c07695fc7a3b6666960f9d38de73eb697cf2a
JuanTasayco <juantasayco266@gmail.com>
2023-07-29 02:57:18 -0500

    addCarrousel

--- src/app/moon/components/carrousel/carrousel.component.ts
+++ src/app/moon/components/carrousel/carrousel.component.ts
@@ -1,20 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Crew } from '../../pages/interfaces/dataMoon.interfaces';
-
+import { gsap } from 'gsap';
 @Component({
   selector: 'app-carrousel',
   templateUrl: './carrousel.component.html',
   styleUrls: ['./carrousel.component.css'],
 })
-export class CarrouselComponent implements OnInit {
+export class CarrouselComponent implements OnInit, AfterViewInit {
+  @ViewChild('containerSlides') slides!: ElementRef<HTMLElement>;
+  @ViewChildren('element') elements!: QueryList<ElementRef>;
+  @ViewChild('element') element!: ElementRef<HTMLElement>;
+  @ViewChildren('barButton') buttonsBar!: QueryList<ElementRef>;
+  private lengthContainers: number = 0;
   crewData: Crew[] = [];
+  currentSlide: number = 0;
 
-  constructor(private dataService: DataService) {}
+  ngAfterViewInit(): void {
+    this.lengthContainers = this.elements.length;
+  }
   ngOnInit(): void {
     this.dataService.getCrew().subscribe((result) => {
       this.crewData = result;
       console.log(this.crewData);
     });
   }
+
+  next() {
+    this.currentSlide =
+      (this.currentSlide + 1 + this.lengthContainers) % this.lengthContainers;
+    this.moveContainerWithGsap(this.currentSlide);
+
+    this.activeColorBarSlide();
+  }
+
+  prev() {
+    this.currentSlide =
+      (this.currentSlide - 1 + this.lengthContainers) % this.lengthContainers;
+    this.moveContainerWithGsap(this.currentSlide);
+    this.activeColorBarSlide();
+  }
+
+  moveContainerWithGsap(posSlide: number) {
+    gsap.to(this.slides.nativeElement, {
+      x: -posSlide * this.element.nativeElement.clientWidth,
+      duration: 0.7,
+      ease: 'power2.inOut',
+    });
+  }
+
+  activeColorBarSlide() {
+    this.buttonsBar.forEach((element, pos, contentArray) => {
+      if (pos == this.currentSlide) {
+        contentArray.forEach((elements) => {
+          elements.nativeElement.classList.remove('act');
+        });
+        element.nativeElement.classList.add('act');
+      }
+    });
+  }
+
+  constructor(private dataService: DataService) {}
 }
