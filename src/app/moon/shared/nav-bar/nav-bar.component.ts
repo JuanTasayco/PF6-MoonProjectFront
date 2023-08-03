b456bd54d7868face2c1a9fae5f6f6063163948c
JuanTasayco <juantasayco266@gmail.com>
2023-08-03 01:41:42 -0500

    addGsapButNeedFixTranslation

--- src/app/moon/shared/nav-bar/nav-bar.component.ts
+++ src/app/moon/shared/nav-bar/nav-bar.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+} from '@angular/core';
+import { gsap } from 'gsap';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
-export class NavBarComponent {
+export class NavBarComponent implements AfterViewInit {
+  /* close navBar menu */
+  @ViewChild('navBarEl') navBarEl!: ElementRef<HTMLElement>;
+  ngAfterViewInit(): void {
+    this.widthVw();
+  }
 
+  closeNavBar() {
+    gsap.to(this.navBarEl.nativeElement, {
+      xPercent: '0',
+      duration: 1,
+    });
+  }
+  openNavBar() {
+    gsap.to(this.navBarEl.nativeElement, {
+      xPercent: '-100',
+    });
+  }
+
+  @HostListener('window:resize', [])
+  widthVw() {
+    console.log(window.innerWidth);
+    if (window.innerWidth < 640) {
+      this.navBarEl.nativeElement.classList.add('navBar');
+    } else {
+      this.navBarEl.nativeElement.classList.remove('navBar');
+    }
+  }
+
+  constructor() {}
 }
