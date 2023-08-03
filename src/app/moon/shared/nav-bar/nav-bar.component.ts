90a51e4bf535e7ffb4b23deadca2cfe997f92112
JuanTasayco <juantasayco266@gmail.com>
2023-08-03 16:03:10 -0500

    finishNavBarLogic

--- src/app/moon/shared/nav-bar/nav-bar.component.ts
+++ src/app/moon/shared/nav-bar/nav-bar.component.ts
@@ -14,29 +14,39 @@ import { gsap } from 'gsap';
 export class NavBarComponent implements AfterViewInit {
   /* close navBar menu */
   @ViewChild('navBarEl') navBarEl!: ElementRef<HTMLElement>;
-  ngAfterViewInit(): void {
-    this.widthVw();
-  }
+  @ViewChild('buttonOpen') btnOpen!: ElementRef<HTMLElement>;
+  ngAfterViewInit(): void {}
 
   closeNavBar() {
+    this.btnOpen.nativeElement.classList.add('z-[100]');
     gsap.to(this.navBarEl.nativeElement, {
-      xPercent: '0',
+      opacity: 0,
       duration: 1,
+      xPercent: 100,
     });
   }
   openNavBar() {
-    gsap.to(this.navBarEl.nativeElement, {
-      xPercent: '-100',
-    });
+    this.btnOpen.nativeElement.classList.remove('z-[100]');
+    gsap.fromTo(
+      this.navBarEl.nativeElement,
+      {
+        xPercent: '100',
+        opacity: 0,
+      },
+      {
+        xPercent: '0',
+        opacity: 1,
+      }
+    );
   }
 
   @HostListener('window:resize', [])
   widthVw() {
     console.log(window.innerWidth);
     if (window.innerWidth < 640) {
-      this.navBarEl.nativeElement.classList.add('navBar');
+      /*  this.navBarEl.nativeElement.classList.add('ocultar'); */
     } else {
-      this.navBarEl.nativeElement.classList.remove('navBar');
+      /*   this.navBarEl.nativeElement.classList.remove('ocultar'); */
     }
   }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -56,7 +56,7 @@ export const theme = {
     subheading1: "28px",
     subheading2: "14px",
     navText: "16px",
-    parrafo : '1.3rem'
+    parrafo: "1.3rem",
   },
 
   letterSpacing: {
